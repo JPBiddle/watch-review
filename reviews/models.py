9bad3ac48cd10e5fe57a3b006bd8b3d48283a837
JPBiddle <joelbiddle@hotmail.co.uk>
2024-03-10 21:54:03 +0000

    still troubleshooting database not working

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,6 @@
 from reviews import db
 
-class Reviews(db.Model):
+class reviews(db.Model):
     # schema for new posted reviews
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.string(60))
@@ -8,5 +8,9 @@ class Reviews(db.Model):
     author = db.Column(db.string(30))
     date = db.column(db.Datetime)
     content = db.Column(Text)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.title
 
 
