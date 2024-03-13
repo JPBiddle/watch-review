769ed0601b6dafcae115f4be4055a397745cc82d
JPBiddle <joelbiddle@hotmail.co.uk>
2024-03-13 20:21:11 +0000

    Changed signup form to flask form

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,7 @@
 from reviews import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, time
-
+from flask_login import UserMixin
 class Reviews(db.Model):
     # schema for new posted reviews
     id = db.Column(db.Integer, primary_key=True)
@@ -15,13 +15,23 @@ class Reviews(db.Model):
         return self.title
 
 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     # schema for new user created
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False)
     emailaddress = db.Column(db.String(150), nullable=False, unique=True)
     password_hash = db.Column(db.String(128))
 
+@property
+def password(self):
+        raise AttributeError('password is incorrect')
 
-    def __repr__(self):
-        return self.username
+@password.setter
+def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+def __repr__(self):
+    return self.username
--- reviews/routes.py
+++ reviews/routes.py
@@ -2,9 +2,14 @@ import os
 from flask import render_template, request, redirect, url_for
 from reviews import app, db
 from .models import Reviews, Users
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, ValidationError
 
 
 app.static_folder = 'static'
+
+# Routes for navigation
 
 @app.route("/")
 def index():
@@ -25,10 +30,14 @@ def review():
     return render_template("review.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    username = None
+    form = SignUp()
+    return render_template("signup.html", name=username, form=form)
 
+
+# Routes to post a review
 
 @app.route("/posts/<string:id>")
 def posts(id):
@@ -36,6 +45,7 @@ def posts(id):
 
     return render_template('posts.html', posts=posts)
     
+# Route for add post form
 
 @app.route("/addpost", methods=['GET', 'POST'])
 def addpost():
@@ -50,3 +60,17 @@ def addpost():
     db.session.commit()
 
     return redirect(url_for('home'))
+
+
+# Routes to register as a new user
+
+class SignUp(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+    def validate_user(self, username):
+        existing_user_username = Users.query.filter_by(username=username.data).first()
+
+        if existing_user_username:
+            raise ValidationError("User already exists.")
